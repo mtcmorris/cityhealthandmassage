8d3335728c1a4f19de7e293c863be6eacda9a5e3
Michael Morris <mtcmorris@gmail.com>
2011-12-15 21:40:01 +1100

    Added Gallery

---
+++ coffee/slider.coffee
@@ -0,0 +1,81 @@
+class Slider
+  constructor: (@target, @options) ->
+    self = this
+    this.target = @target
+    this.options = @options
+    this.loader = new Loader(this)
+    this.timer = new Timer(this)
+    this.timer.start()
+
+  show: (@index) ->
+    this.loader.load(index)
+
+  setTitle: (text) ->
+    self = this
+    $(this.options.title_target).fadeOut 250, ->
+      $(this).html(text)
+      $(self.options.title_target).fadeIn(250)
+
+
+class Timer
+  constructor: (@parent) ->
+    this.parent = @parent
+    this.tick = null
+
+  start: ->
+    self = this;
+    this.tick = setInterval((-> self.parent.loader.next()), 8000)
+
+  stop: ->
+    self = this;
+    if this.tick != null
+      clearInterval(self.tick)
+
+
+class Loader
+  constructor: (@parent) ->
+    this.parent = @parent
+    this.slides = $('figure', this.parent.target)
+
+  next: ->
+    self = this;
+    currentSlide = $('figure:visible', $(self.parent.target))
+    if currentSlide.next().length == 1
+      next = currentSlide.next()
+    else
+      next = this.slides.first()
+    console.log next
+    self.transition(currentSlide, next)
+
+  transition: (current, target) ->
+    self = this;
+    self.parent.isAnimating = true
+    if current.length > 0
+      $(target).show()
+      $(this.parent.target).css('height', $(target).height())
+      $(target).css({'zIndex':'1'})
+      $(current).fadeOut 1500, ->
+        self.parent.isAnimating = false
+        $(target).css({'zIndex':'2'})
+        $(this).parent().css({'zIndex':'1'})
+    else
+      $(target).fadeIn 1500, ->
+        self.parent.isAnimating = false
+      $(target).css({'zIndex':'2'})
+
+$ ->
+  $.fn.slider = (options) ->
+    SliderOptions = {
+        'pagination_target':null,
+        'title_target':null,
+        'indicatorFadeInSpeed':400,
+        'indicatorFadeOutSpeed':200
+    }
+    options = $.extend({}, SliderOptions, options)
+    return this.each ->
+        new Slider(this, options)
+
+  if $('#gallery').length != 0
+    $('#gallery').slider()
+  if $('#tagline').length != 0
+    $('#tagline').slider()
---
+++ js/slider.js
@@ -0,0 +1,134 @@
+(function() {
+  var Loader, Slider, Timer;
+
+  Slider = (function() {
+
+    function Slider(target, options) {
+      var self;
+      this.target = target;
+      this.options = options;
+      self = this;
+      this.target = this.target;
+      this.options = this.options;
+      this.loader = new Loader(this);
+      this.timer = new Timer(this);
+      this.timer.start();
+    }
+
+    Slider.prototype.show = function(index) {
+      this.index = index;
+      return this.loader.load(index);
+    };
+
+    Slider.prototype.setTitle = function(text) {
+      var self;
+      self = this;
+      return $(this.options.title_target).fadeOut(250, function() {
+        $(this).html(text);
+        return $(self.options.title_target).fadeIn(250);
+      });
+    };
+
+    return Slider;
+
+  })();
+
+  Timer = (function() {
+
+    function Timer(parent) {
+      this.parent = parent;
+      this.parent = this.parent;
+      this.tick = null;
+    }
+
+    Timer.prototype.start = function() {
+      var self;
+      self = this;
+      return this.tick = setInterval((function() {
+        return self.parent.loader.next();
+      }), 8000);
+    };
+
+    Timer.prototype.stop = function() {
+      var self;
+      self = this;
+      if (this.tick !== null) return clearInterval(self.tick);
+    };
+
+    return Timer;
+
+  })();
+
+  Loader = (function() {
+
+    function Loader(parent) {
+      this.parent = parent;
+      this.parent = this.parent;
+      this.slides = $('figure', this.parent.target);
+    }
+
+    Loader.prototype.next = function() {
+      var currentSlide, next, self;
+      self = this;
+      currentSlide = $('figure:visible', $(self.parent.target));
+      if (currentSlide.next().length === 1) {
+        next = currentSlide.next();
+      } else {
+        next = this.slides.first();
+      }
+      console.log(next);
+      return self.transition(currentSlide, next);
+    };
+
+    Loader.prototype.transition = function(current, target) {
+      var self;
+      self = this;
+      self.parent.isAnimating = true;
+      if (current.length > 0) {
+        $(target).show();
+        $(this.parent.target).css('height', $(target).height());
+        $(target).css({
+          'zIndex': '1'
+        });
+        return $(current).fadeOut(1500, function() {
+          self.parent.isAnimating = false;
+          $(target).css({
+            'zIndex': '2'
+          });
+          return $(this).parent().css({
+            'zIndex': '1'
+          });
+        });
+      } else {
+        $(target).fadeIn(1500, function() {
+          return self.parent.isAnimating = false;
+        });
+        return $(target).css({
+          'zIndex': '2'
+        });
+      }
+    };
+
+    return Loader;
+
+  })();
+
+  $(function() {
+    $.fn.slider = function(options) {
+      var SliderOptions;
+      SliderOptions = {
+        'pagination_target': null,
+        'title_target': null,
+        'indicatorFadeInSpeed': 400,
+        'indicatorFadeOutSpeed': 200
+      };
+      options = $.extend({}, SliderOptions, options);
+      return this.each(function() {
+        return new Slider(this, options);
+      });
+    };
+    if ($('#gallery').length !== 0) $('#gallery').slider();
+    if ($('#tagline').length !== 0) return $('#tagline').slider();
+  });
+
+}).call(this);
